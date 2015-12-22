a4e17ab0934655635fd52482eb99f9c3019d6e2b
Martin Raldholm <martin@raldholm.se>
2015-12-22 09:07:24 +0100

    finished moreThanDouble

---
+++ LAB1/LAB1/Scripts/array.js
@@ -0,0 +1,8 @@
+﻿/* 
+
+I en vektor finns ett personnummer.
+Rita en algoritm som kontrollerar att födelsedagsdatum
+och de fyra sista åtskiljs med ett bindestreck.
+Skriv ut ett felmeddelande om så inte är fallet.
+
+*/
--- LAB1/LAB1/Scripts/twoNumbers.js
+++ LAB1/LAB1/Scripts/twoNumbers.js
@@ -5,3 +5,38 @@ Testa om det första talet är mer än dubbelt så stort som det andra talet.
 I så fall ska meddelandet bli: ”För stort tal”.
 
 */
+
+var calculate = document.getElementById("calculatebutton");
+
+calculate.addEventListener('click', calcFunction, false);
+
+function calcFunction() {
+
+    var tal1 = Number(document.getElementById("tal1").value);
+    var tal2 = Number(document.getElementById("tal2").value);
+    
+    if (isNaN(tal1 || tal2)) {
+        alert("Felaktigt tecken inmatat!");
+    } 
+    else
+    {
+        var answer = moreThanDouble(tal1, tal2);
+
+        if ( answer === true) 
+        {
+            document.getElementById("result").value = "För stort tal";
+        } 
+        else {
+            document.getElementById("result").value = answer.toString() + " = OK!";
+        }
+    }
+}
+
+function moreThanDouble(tal1, tal2) {
+    if (tal1 > (tal2*2))
+    {
+        return true;
+    }
+
+    return false;
+}
