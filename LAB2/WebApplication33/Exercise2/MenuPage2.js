ee9867db482ede3a26bfdb7b2e67443ab60965b5
Martin Raldholm <martin@raldholm.se>
2016-01-03 17:21:07 +0100

    created 7pizza, Subs and drinks in productarrays

--- LAB2/WebApplication33/Exercise2/MenuPage2.js
+++ LAB2/WebApplication33/Exercise2/MenuPage2.js
@@ -67,11 +67,38 @@ var HamburgerProductsArray = [
 ];
 
 //3. add 7 drinks to the page using Product.
+var drinksProductsArray = [
+    new Product("Beer", "50cl", "Monday", 9.90),
+    new Product("White wine", "25cl", "Thuesday", 11.90),
+    new Product("Red Wine", "25cl", "Wednesday", 11.90),
+    new Product("Tequila", "6cl", "Thursday", 5.90),
+    new Product("Whiskey", "8cl", "Friday", 13.90),
+    new Product("Water", "50cl", "Saturday", 0.90),
+    new Product("Milk", "50cl", "Sunday", 2.90)
+];
 
 //4. add 7 pizzas to the page using Product.
 
+var pizzaProductsArray = [
+    new Product("Hawaii", "400g", "Monday", 19.90),
+    new Product("Margarita", "400g", "Thuesday", 19.90),
+    new Product("Calzone", "400g", "Wednesday", 19.90),
+    new Product("Caprisiossa", "400g", "Thursday", 19.90),
+    new Product("Kebab pizza", "450g", "Friday", 21.90),
+    new Product("Olympia Special", "520g", "Saturday", 21.90),
+    new Product("Husets Pizza", "500g", "Sunday", 20.90)
+];
 //5. add 7 subs to the page using Product.
 
+var subsProductsArray = [
+    new Product("Chicken Sub", "180g", "Monday", 13.90),
+    new Product("Turkey Sub", "200g", "Thuesday", 13.90),
+    new Product("Cheese Sub", "150g", "Wednesday", 13.90),
+    new Product("Ham Sub", "200g", "Thursday", 13.90),
+    new Product("Veg. Sub", "180g", "Friday", 13.90),
+    new Product("Kebab Sub", "220g", "Saturday", 13.90),
+    new Product("Meat Sub", "200g", "Sunday", 13.90)
+];
 //6. GO to file FixIt.html
 //DRINKS
 //Subs
